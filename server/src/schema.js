2975edd5a3b1227a1f87fa11f18fd7d309b24851
amartiniuc <amartiniuc@griddynamics.com>
2023-05-08 11:30:08 +0300

    Add server/client logic

---
+++ server/src/schema.js
@@ -0,0 +1,25 @@
+const gql = require("graphql-tag");
+
+const typeDefs = gql`
+  "Query to get tracks array for the homepage grid"
+  type Query {
+    tracksForHome: [Track!]!
+  }
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    moduleCount: Int
+  }
+  "Author of a complete Track"
+  type Author {
+    id: ID!
+    name: String!
+    photo: String
+  }
+`;
+
+module.exports = typeDefs;
